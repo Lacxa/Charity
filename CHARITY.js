2e25986e69eae1f2773bdaa15b358a35486ea32f
Alphonce <83605282+Lacxa@users.noreply.github.com>
2025-08-20 00:21:48 +0300

    charity uno
    Dos

---
+++ CHARITY.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","You know what you should do?","Save Chirldren Save The World!","Valunteer for helping chirldren!", "Donate to our foundation!", "Have a great day!"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
